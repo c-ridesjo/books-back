60ed2e064a5b12e5253bbcc3558caef446e49b1c
Camilla Ridesjö <camilla.ridesjo@gmail.com>
2023-03-10 13:48:04 +0100

    Add books and users

---
+++ routes/library.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var router = express.Router();
+
+let books =[
+    {id: 1, title: "Napalm", author: "Frida Moisto", borrowed: false},
+    {id: 1, title: "Spindeln", author: "Lars Kepler", borrowed: false},
+    {id: 1, title: "Midnattsdåd", author: "Nora Roberts", borrowed: false},
+    {id: 1, title: "Lazarus", author: "Lars Kepler", borrowed: false},
+    {id: 1, title: "Pepparkakshuset", author: "Carin Gerhardsen", borrowed: false},
+]
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.json('books');
+});
+
+router.post('/', function(req, res, next) {
+
+    let newBook = req.body;   //hämtar värdet som är sparat i body och sparar det objektet i newBook
+    console.log(req.body);
+    newBook.id = books.length + 1;
+    books.push(newBook);
+    console.log(newBook);  
+    res.json(books);           // skickar books-arrayen som svar                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+  });
+
+router.get('/:bookId', function(req, res, next){   // next måste inte vara med, däremot reqest och response
+  let bookId = req.params.bookId;
+  console.log(bookId);
+
+  let findBook = books.find(books => books.id == bookId);
+  res.json(findBook);
+});
+
+router.post('/borrowed', function(req, res, next) {
+    console.log("body", req.body);
+    let bookId = req.body.bookId;
+    console.log(bookId);
+
+    let borrowedBook = books.find(books => books.id == bookId);
+    borrowedBook.borrowed =!borrowedBook.borrowed;
+    console.log("book borrowed", borrowedBook);
+    console.log("all books", books);
+    res.json(books);
+});
+
+
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+let users = [
+  {id: 1, name: 'John'},
+  {id: 2, name: 'Jane'},
+  {id: 3, name: 'Bob'},
+  {id: 4, name: 'Alice'},
+  {id: 5, name: 'Harry'}
+]
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json('users');      // skickar users-arrayen som svar
 });
+
+router.post('/', function(req, res, next) {
+
+  let newUser = req.body;   //hämtar värdet som är sparat i body och sparar det objektet i newUser
+  newUser.id = users.length + 1;
+  users.push(newUser);
+  console.log(newUser);
+
+  res.json({users});           // skickar users-arrayen som svar                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+});
+
+router.get('/test', function(req, res, next){
+  res.send('testroutern');
+}); 
 
 module.exports = router;
